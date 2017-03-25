fc2bf8d6a8018b927a5b901f7927844a14f32b5c
BettinaP <bettinaprophete@gmail.com>
2017-03-25 19:54:39 -0400

    site formatted

---
+++ app/models/person.rb
@@ -0,0 +1,7 @@
+class Person < ApplicationRecord
+    validates :name, presence: true,
+                     length: { minimum: 2 }
+                     validates :favoritecity, presence: true,
+                     length: { minimum: 2 }
+                     
+end
