fa3fae4f0f64fa6446e4150cb1df742e9fcf62fb
BettinaP <bettinaprophete@gmail.com>
2017-03-25 20:14:02 -0400

    checked logs

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,6 @@
 class PeopleController < ApplicationController
   
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   
   def index
       @people = Person.all
