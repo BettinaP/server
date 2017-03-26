bf8e404c90dec7d88cac9ccb556eaaa4c27ccf69
BettinaP <bettinaprophete@gmail.com>
2017-03-25 20:28:23 -0400

    erorr handling

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ApplicationRecord
     validates :name, presence: true,
                      length: { minimum: 2 }
-                     validates :favoritecity, presence: true,
+    validates :favoritecity, presence: true,
                      length: { minimum: 2 }
                      
 end
